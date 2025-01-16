87cdfca6f19d9dd83b06dc4d68c49481e6fdbec6
Abhi Desai <aid@taglineinfotech.com>
2025-01-16 17:07:04 +0530

    Crud operation with save and edit data

--- src/presentation/CrudOperation.jsx
+++ src/presentation/CrudOperation.jsx
@@ -1,107 +1,121 @@
-import { useEffect, useState } from "react";
-import { Button, IconButton, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
+import { useState } from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import validation from "../utils/validation";
 
 const CrudOperation = () => {
   const [formData, setFormData] = useState({});
+  const [open, setOpen] = useState({});
+
   const [error, setError] = useState({});
-  const [dataRow, setDataRow] = useState([]); 
-  const [editIndex, setEditIndex] = useState(null); 
+  const [dataRow, setDataRow] = useState(() => {
+    const storedData = localStorage.getItem("item");
+    return storedData ? JSON.parse(storedData) : [];
+  });
+  const [editIndex, setEditIndex] = useState(null);
 
   const formAttribute = [
     {
       name: "name",
       label: "Full Name",
-      pattern: 'checkName',
+      pattern: "checkName",
       type: "text",
     },
     {
       name: "email",
       label: "Email",
       type: "email",
-      pattern: 'checkEmail',
+      pattern: "checkEmail",
     },
     {
       name: "number",
       label: "Mo. number",
       type: "text",
-      pattern: 'checkMobileNumber',
+      pattern: "checkMobileNumber",
     },
   ];
-
-
+  
   const handleSaveData = (e) => {
     e.preventDefault();
-    let newErr = {}
-
-  // EMPTY FIELD CHECK KRVA 
-   formAttribute.forEach((item) => { 
-   newErr[item.name] = validation(item.name, item.pattern, formData[item.name]) 
-
-  })
-  
-  setError((prev) => ({...prev, ...newErr }))
-
-   if(Object?.values(newErr)?.every((el) => el === undefined)) {    
-     if (!editIndex) {
-      dataRow.push(formData)
-      localStorage.setItem('item', JSON.stringify(dataRow));
-     } else{
-       const updatedData = [...dataRow];
-       updatedData[editIndex] = formData;
-       setDataRow(updatedData);
-       setEditIndex(null);      
-     }
-     setFormData({})  
-   }
-   
-
-  };
-  
-  const handleDelete = (index) => {  
-    dataRow.splice(index,1);    
-    setDataRow([...dataRow]);
-    setError({})
+    let newErr = {};
+    
+    // EMPTY FIELD CHECK KRVA
+    formAttribute.forEach((item) => {
+      newErr[item.name] = validation(item.name, item.pattern, formData[item.name]);
+    });
+    
+    setError((prev) => ({ ...prev, ...newErr }));
+    
+    if (Object?.values(newErr)?.every((el) => el === undefined)) {
+      
+      if (editIndex === null || editIndex === undefined) {
+        dataRow.push(formData);
+        localStorage.setItem("item", JSON.stringify(dataRow));
+      } else {
+        const updatedData = [...dataRow];
+        updatedData[editIndex] = formData;
+        setDataRow(updatedData);
+        localStorage.setItem("item", JSON.stringify(updatedData));
+        setEditIndex(null);
+      }
+      setFormData({});
+    }
   };
 
+  const handleDelete = (index) => {
+    const updatedData = dataRow.filter((_, i) => i !== index);
 
-  const handleEdit = (index) => { 
+    setDataRow(updatedData);
+    localStorage.setItem("item", JSON.stringify(updatedData));
+
+    setError({});
+    setFormData({});
+    setOpen({show: false});
+    setEditIndex(null);
+  };
+
+  const handleEdit = (index) => {
+    console.log("editIndex", editIndex);
     setFormData(dataRow[index]);
-    setEditIndex(index); 
-    setError({})
+    setEditIndex(index);
+    setError({});
+    console.log("editIndex", index);
   };
 
   const handleChange = (e, pattern) => {
-    const {name,value} = e.target     
-    setFormData((prev) => ({ ...prev, [name]: value }))
-    setError((prev) => ({...prev, [name]: validation(name, pattern, value)  }))
-  }
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+    setError((prev) => ({ ...prev, [name]: validation(name, pattern, value) }));
+  };
 
-  
-  
-  
+
+  const handleClickOpen = (i) => {
+    setOpen({show: true, id:i});
+  };
+  const handleClose = () => {
+    setOpen({show: false});
+  };
 
   return (
     <>
       <Stack p={3}>
         <form onSubmit={handleSaveData}>
           <Stack spacing={3} direction="row" mb={4}>
-            {formAttribute?.map((val,i) => (
+            {formAttribute?.map((val, i) => (
               <TextField
                 key={i}
                 type={val?.type || "text"}
                 label={val?.label}
                 name={val?.name}
-                value={formData[val?.name] || ''}
+                value={formData[val?.name] || ""}
                 variant="standard"
-                onChange={(e)=> handleChange(e, val.pattern)}
+                onChange={(e) => handleChange(e, val.pattern)}
                 helperText={error?.[val?.name]}
                 error={error?.[val?.name]}
               />
             ))}
-            <Button variant="contained" type="submit" sx={{height:'100%'}}>
+            <Button variant="contained" type="submit" sx={{ height: "100%" }}>
               {editIndex !== null ? "Update" : "Save"}
             </Button>
           </Stack>
@@ -109,10 +123,10 @@ const CrudOperation = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Mobile No.</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Action</TableCell>
+              <TableCell sx={{fontWeight:700, fontSize:18}}>Name</TableCell>
+              <TableCell sx={{fontWeight:700, fontSize:18}}>Mobile No.</TableCell>
+              <TableCell sx={{fontWeight:700, fontSize:18}}>Email</TableCell>
+              <TableCell sx={{fontWeight:700, fontSize:18}}>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -122,10 +136,10 @@ const CrudOperation = () => {
                 <TableCell>{obj?.number}</TableCell>
                 <TableCell>{obj?.email}</TableCell>
                 <TableCell>
-                  <IconButton onClick={()=>handleEdit(i)}>
+                  <IconButton onClick={() => handleEdit(i)} color="primary">
                     <ModeEditIcon />
                   </IconButton>
-                  <IconButton onClick={()=>handleDelete(i)}>
+                  <IconButton onClick={()=>handleClickOpen(i)} color="error">
                     <DeleteOutlineIcon />
                   </IconButton>
                 </TableCell>
@@ -134,6 +148,24 @@ const CrudOperation = () => {
           </TableBody>
         </Table>
       </Stack>
+       <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open.show}
+        maxWidth="lg"
+      >
+        <DialogContent dividers>
+          <Typography fontWeight={400} color="error">Are you sure want to delete?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" autoFocus onClick={handleClose}>
+            No
+          </Button>
+          <Button variant="contained" autoFocus onClick={()=> handleDelete(open.id)}>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
